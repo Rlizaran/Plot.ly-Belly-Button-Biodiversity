d6c0e11d0b4bdb6848dda3e2e7b0b6ae89fe8d27
Rodrigo Lizaran <rlizaran97@gmail.com>
2021-11-16 13:34:40 -0800

    Completed

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -18,6 +18,70 @@ function createMetadata(sample) {
     })
 }
 
+// Function to create Bar chart and Bubble Chart
+function createPlots(sample) {
+    // Use d3.json to read samples data for the plots
+    d3.json("samples.json").then((data) => {
+        var samples = data.samples;
+
+        // Filter by id selected to get samples values
+        var sampleArray = samples.filter(sampleObject => sampleObject.id == sample);
+        var result = sampleArray[0]
+
+        var values = result.sample_values; // Values for chart
+        var hovertext_label = result.otu_labels; // Labels for hovertext 
+        var labels = result.otu_ids; // Labels for chart
+        
+        console.log(values)
+        console.log(labels)
+
+        // Create bar_data, Use slice to get top 10 OTUs for each sample
+        var bar_data = [
+            {
+                x: values.slice(0,10).reverse(),
+                y: labels.slice(0,10).map(otuID => `OTU ${otuID}`).reverse(),
+                hovertext: hovertext_label.slice(0,10).reverse(),
+                hoverinfo: "hovertext",
+                type: "bar",
+                orientation: "h"
+            }
+        ];
+        console.log(bar_data)
+        // Create bar_layout
+        var bar_layout = {
+            title: "Top 10 OTUs",
+            margin: {t:30, l:120}
+        };
+        console.log(bar_layout)
+        // Create Plot at div with id "bar"
+        Plotly.newPlot("bar", bar_data, bar_layout);
+    
+
+    //  Create data for bubble
+    var bubble_data = [
+        {
+            x:labels,
+            y: values,
+            text: hovertext_label,
+            mode: "markers",
+            marker: {
+                color: labels,
+                size: values
+            }
+        }
+    ];
+
+    // Create layout for bubble
+    var bublle_layout = {
+        margin: {t: 0},
+        xaxis: {title: "OTU ID"},
+        hobermode: "closest"
+    };
+
+    Plotly.newPlot("bubble", bubble_data, bublle_layout);
+    });
+}
+
 // Init function
 function init() {
     // Dropdown select element for the dataset
@@ -35,15 +99,15 @@ function init() {
         // Set first sample for initial plot
         const firstSample = names[0];
         createMetadata(firstSample);
-        // createPlots(firstSample);
-    })
+        createPlots(firstSample);
+    });
 }
 
-// option Change function to change sample selected and update webpage
+// Option Change function to change sample selected and update webpage
 function optionChanged(newSample) {
     // Get new data each time a new sample is selected from the dropdown buttom
     createMetadata(newSample);
-    // createPlots(newSample);
+    createPlots(newSample);
 }
 
 
